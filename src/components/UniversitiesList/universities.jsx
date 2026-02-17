93295603aef2f7842f70f44ed5fbc78681e4eab6
eliott-colin <eliott.colin.pro@gmail.com>
2026-02-17 14:31:25 +0100

    fix/path-image : new path added on all image /finalfocus/

--- src/components/UniversitiesList/universities.jsx
+++ src/components/UniversitiesList/universities.jsx
@@ -3,6 +3,8 @@ import "./universities.css"
 import Title from "../title/title.jsx";
 
 function UniversitiesList({universities}) {
+    const baseUrl = import.meta.env.BASE_URL;
+    
     return (
         <div>
             <Title title="UNIVERSITES PARTENAIRES" color="linear-gradient(89deg, #F80DFD -0.19%, #8B1EDF 103.13%)" colorFont="#FFFFFF00" fontSize="24px" arrow={true} />
@@ -10,7 +12,7 @@ function UniversitiesList({universities}) {
                 {universities.map((item, index) => (
                     <div key={index}>
                         <div className="image-wrapper">
-                            <img src={`/${item.image}`} alt={item.name}/>
+                            <img src={`${baseUrl}${item.image}`} alt={item.name}/>
                         </div>
                         <p>{item.name}</p>
                         <p className="smallText">{item.ville} - {item.postalCode}</p>
