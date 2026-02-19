ac319bceef1c0f4f2f5fc2bb2dead86f044855e3
Eduard Baescu <eduard@geev.com>
2026-02-19 11:36:46 +0100

    Fixes

--- src/components/Team/team.jsx
+++ src/components/Team/team.jsx
@@ -1,20 +1,19 @@
 import React from "react";
 import "./team.css";
 
-function TeamComponent(data) {
-  console.log(data)
+function TeamComponent({data}) {
   return (
     <div className="team-wrapper">
       <div className="team-image-wrapper">
-        <img src={data.data.image} alt={data.data.name} />
+        <img src={data.image} alt={data.name} />
       </div>
       <div className="team-text-wrapper">
-        <h2>{data.data.name}</h2>
-        <p>{data.data.members}</p>
+        <h2>{data.name}</h2>
+        <p>{data.members}</p>
       </div>
       <div
         className="team-game-wrapper"
-        style={{ backgroundImage: `url('${data.data.imageGame}')` }}
+        style={{ backgroundImage: `url('${data.imageGame}')` }}
       ></div>
     </div>
   );
