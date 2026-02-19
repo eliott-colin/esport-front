0dff24eb945427204224c5072b4321e893716f9a
eliott-colin <eliott.colin.pro@gmail.com>
2026-02-19 10:54:45 +0100

    issue 22 fix : add new link on each team

--- src/components/Teams/teams.jsx
+++ src/components/Teams/teams.jsx
@@ -1,13 +1,23 @@
 import TeamComponent from "../Team/team";
 import "./teams.css";
+import { Link } from "react-router-dom";
 
 function Teams() {
   return (
     <div className="teams-wrapper">
-      <TeamComponent image="./l.png" name="Team Alpha" members="5" />
-      <TeamComponent image="./lol1.png" name="Team Beta" members="5" />
-      <TeamComponent image="./logo.png" name="Team Gamma" members="5" />
-      <TeamComponent image="./logo.png" name="Team Gamma" members="5" />
+      {[
+        { image: "team1.png", name: "Team 1", members: "5 members" , imageGame: "f.png"},
+        { image: "team2.png", name: "Team 2", members: "4 members" , imageGame: "f.png"},
+        { image: "team3.png", name: "Team 3", members: "6 members" , imageGame: "f.png"},
+        { image: "team1.png", name: "Team 4", members: "5 members" , imageGame: "f.png"},
+        { image: "team2.png", name: "Team 5", members: "4 members" , imageGame: "f.png"},
+        { image: "team3.png", name: "Team 6", members: "6 members" , imageGame: "f.png"},
+      ].map((element, index) => (
+        
+        <Link to={`/finalfocus/teams/${index}`} style={{textDecoration: "none"}}>
+          <TeamComponent key={index} data={element} />
+        </Link>
+      ))}
     </div>
   );
 }
