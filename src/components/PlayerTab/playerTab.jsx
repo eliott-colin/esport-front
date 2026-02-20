b430d40b0e756d48bccba60ff8e97dcb959eb139
eliott-colin <eliott.colin.pro@gmail.com>
2026-02-20 10:49:12 +0100

    push

---
+++ src/components/PlayerTab/playerTab.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./playerTab.css";
+
+function PlayerTab({data}) {
+  return (
+    <div className="player-wrapper">
+      <div className="player-image-wrapper">
+        <img src={'/finalfocus/' + data.image} alt={data.name} />
+      </div>
+      <div className="player-text-wrapper">
+        <p>{data.name}</p>
+      </div>
+      <div
+        className="player-game-wrapper"
+        style={{ backgroundImage: `url('/finalfocus/${data.imageGame}')` }}
+      ></div>
+    </div>
+  );
+}
+
+export default PlayerTab;
--- src/pages/teamDetails/teamDetails.jsx
+++ src/pages/teamDetails/teamDetails.jsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import ProfileHeader from "../../components/ProfileHeader/profileHeader.jsx";
+import SegmentedTabs from "../../components/SegmentedTabs/segmentedTabs.jsx";
+import Spacer from "../../components/Spacer/spacer.jsx";
+import PlayersList from "../../components/PlayersList/playersList.jsx";
+
+const TEAMS_TABS = [
+  { id: "informations", label: "Informations" },
+  { id: "teams", label: "Teams", disabled: true },
+  { id: "matchs", label: "Matchs", disabled: true },
+  { id: "succes", label: "Succès", disabled: true },
+];
+const playersData = [
+  { image: "lol1.png", name: "Player 1", members: "5 members" , imageGame: "point.png"},
+  { image: "lol2.png", name: "Player 2", members: "4 members" , imageGame: "point.png"},
+];
 
 function TeamDetails() {
+  const [activeTab, setActiveTab] = useState("informations");
   return (
-    <div>
-      <h1>Détails de l'équipe</h1>
+    <div className="navBarSpace" style={{paddingTop : "0", maxWidth: "100%"}}>
+      <ProfileHeader props={{ bigImage: "bg.png", smallImage: "lol1.png", name: "Los Diaboles" }} />
+      <SegmentedTabs tabs={TEAMS_TABS} value={activeTab} onChange={setActiveTab} />
+      <Spacer size="24px" />
+      {/* {activeTab === "informations" && <PlayersList />} */}
+      <PlayersList data={playersData} />
 
     </div>
   );
